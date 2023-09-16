06cdd4f5f8af9470574baeb5c7ee23c74b8f87b3
Vanessa Eberhard <vanessa.eberhard@students.bfh.ch>
2023-09-16 23:29:36 +0200

    Updates

--- hotelfan-analysis/dataset-reading.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-dat = pd.read_json("dataset/hotelplancase/Hackzurich/de.json")
-
-print(dat)
--- hotelfan-frontend/src/utils/helpers.js
+++
@@ -1,5 +0,0 @@
-export const getPlainText = (text) => {
-    let baseString = text.replace("<p>", "");
-    baseString = baseString.replace("</p>", "");
-    return baseString;
-};
